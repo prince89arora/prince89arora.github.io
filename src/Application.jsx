088a5aefc34bcd4b1190f32eec4c278d606a0378
Prince Arora <aroraprince151@gmail.com>
2022-05-28 18:20:13 +0530

    initial setup

---
+++ src/Application.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Application = () => {
+    return (
+        <>
+            <p>Application for profile page</p>
+        </>
+    )
+}
+
+export default Application;
